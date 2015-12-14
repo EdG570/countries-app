8797c541044afa39153422fae9b6c25c414455a1
Ed Glazenski <edglazenski@gmail.com>
2015-12-13 22:03:23 -0500

    Added http requests for details page. Also added some initial styling.

--- app/cc-app.js
+++ app/cc-app.js
@@ -20,8 +20,10 @@
     })
 
     .factory('countryData', function($http, $q) {
+        
         var countryArray = [];
         var defer = $q.defer();
+
         return {
           getCountries: function() {
             
@@ -39,26 +41,94 @@
                   countryArray = results.data.geonames;
                   defer.resolve(countryArray);
               });
-
-              console.log(defer.promise);
               
               return defer.promise; 
 
-            },
+          },
+          
+          search: function(code) {
+               var i = 0;
 
-              search: function(arr, code) {
-                for(var i = 0; i < arr.length; i++) {
-                  if(arr[i].countryCode === code) {
-                    return arr[i];
-                  }
-                }
-                
-              }
+               while(countryArray[i].countryCode !== code && i < countryArray.length) {
+                  i = i + 1;
+               }
+
+               if(i < countryArray.length) {
+                  return countryArray[i];
+               } 
+
+               else {
+                  return null;
+               }
+          }
+        };
+    })
+  
+    .factory('neighborData', function($http, $q) {
+          var neighbors = [];
+          var d = $q.defer();
+
+        return {
+
+          getNeighbors: function(code) {
+            
+              var request = {
+                username: 'cole570',
+                type: 'json',
+                country: code
+              };
+
+              $http({
+                  url: 'http://api.geonames.org/neighbours',
+                  method: 'GET',
+                  params: request
+              })
+              .then(function(results){
+                  neighbors = results.data.geonames;
+                  d.resolve(neighbors); 
+              });
+              
+            return d.promise;  
+
+          }
+        };
+    })
+
+    .factory('capitalData', function($http, $q) {
+        
+        var capObj = {};
+        var d = $q.defer();
+
+        return {
+          
+          getCapital: function(capital) {
+              
+              var request = {
+                username: 'cole570',
+                q: capital,
+                name_equals: capital,
+                isNameRequired: true,
+                type: 'JSON'
+              };
+
+              $http({
+                  url: 'http://api.geonames.org/search',
+                  method: 'GET',
+                  params: request
+              })
+              .then(function(results){
+                 capObj = results.data.geonames[0];
+                 d.resolve(capObj);
+                 
+              });
+          
+              return d.promise;
+
+          }
+          
         }; 
     })
 
-    
-    
     .controller('HomeCtrl', [ function() {
        
     }])
@@ -66,27 +136,39 @@
     .controller('CountryCtrl', [ 'countryData', '$scope', '$location', function(countryData, $scope, $location) {
          countryData.getCountries().then(function(data) {
             $scope.countries = data;
-            console.log(data);
+            
          });
 
          $scope.getDetails = function(code) {
             $location.path('/countries/' + code);
          };
-
-
     }])
 
-    .controller('DetailCtrl', [ 'countryData', '$scope', '$routeParams', function(countryData, $scope, $routeParams) {
+    .controller('DetailCtrl', [ 'countryData', 'capitalData', 'neighborData', '$scope', '$routeParams', '$location', function(countryData, capitalData, neighborData, $scope, $routeParams, $location) {
         
         $scope.countryCode = $routeParams.country;
+        $scope.country = countryData.search($scope.countryCode);
+
         
-        countryData.getCountries().then(function(data) {
-            $scope.countries = data;
-            $scope.detailsInfo = countryData.search();
-            $scope.detailsInfo(data, $scope.countryCode);
-            console.log($scope.detailsInfo);
-         });
+        capitalData.getCapital($scope.country.capital).then(function(cap) {
+            $scope.capital = cap;
+            
+        });
+
+        neighborData.getNeighbors($scope.countryCode).then(function(neighbors) {
+            $scope.neighbors = neighbors;
+            
+        });
+
+        $scope.countryImg = 'http://www.geonames.org/img/country/250/' + $scope.countryCode + '.png';
+
+        $scope.countryCodeLower = $scope.countryCode.toLowerCase();
+
+        $scope.flagUrl = 'http://www.geonames.org/flags/x/' + $scope.countryCodeLower + '.gif';
+
+        $scope.getDetails = function(code) {
+            $location.path('/countries/' + code);
+         };
 
     }]);
-
 })();
